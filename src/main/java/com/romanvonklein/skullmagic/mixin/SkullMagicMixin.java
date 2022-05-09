f1caf357bd36dd1499f0540e5d2118543bb0c787
Roman Kravanja <roman.m.kravanja@student.fh-kiel.de>
2022-05-09 13:26:00 +0200

    Drops by monsters killed by player now added from config file.

--- src/main/java/com/romanvonklein/skullmagic/mixin/SkullMagicMixin.java
+++ src/main/java/com/romanvonklein/skullmagic/mixin/SkullMagicMixin.java
@@ -1,16 +1,21 @@
 package com.romanvonklein.skullmagic.mixin;
 
+import java.util.Map;
+import java.util.Random;
+
 import com.romanvonklein.skullmagic.SkullMagic;
+import com.romanvonklein.skullmagic.config.Config;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 @Mixin(PlayerEntity.class)
 public abstract class SkullMagicMixin extends EntityMixin {
@@ -18,9 +23,18 @@ public abstract class SkullMagicMixin extends EntityMixin {
 	@Inject(at = @At("HEAD"), method = "onKilledOther(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfo;)V")
 	private void onKilledOther(net.minecraft.server.world.ServerWorld world, net.minecraft.entity.LivingEntity other,
 			CallbackInfo info) {
-		SkullMagic.LOGGER.info("Player killed a thing(" + other.getName() + ")!");
-		// PlayerEntity ent = world.getClosestPlayer(other, 5.0);
-		world.spawnEntity(new ItemEntity(world, this.getPos().x, this.getPos().y, this.getPos().z,
-				new ItemStack(Items.MELON_SLICE)));
+		String killedType = EntityType.getId(other.getType()).toString();
+		if (Config.getConfig().drops.containsKey(killedType)) {
+			Map<String, Float> itemDrops = Config.getConfig().drops.get(killedType);
+			Random rand = new Random();
+			for (String itemIdentifyer : itemDrops.keySet()) {
+				Float roll = rand.nextFloat();
+				Float chance = itemDrops.get(itemIdentifyer);
+				if (roll <= chance) {
+					other.dropStack(new ItemStack(Registry.ITEM.get(Identifier.tryParse(itemIdentifyer))));
+				}
+			}
+		}
+
 	}
 }
