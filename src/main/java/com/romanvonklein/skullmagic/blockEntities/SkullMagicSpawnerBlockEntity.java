49337adb5c5e3eee0f62f7f22bb37bfeac4d35c6
Roman Kravanja <roman.m.kravanja@student.fh-kiel.de>
2023-11-06 11:37:29 +0100

    used new spawners in overworld dungeons.

--- src/main/java/com/romanvonklein/skullmagic/blockEntities/SkullMagicSpawnerBlockEntity.java
+++ src/main/java/com/romanvonklein/skullmagic/blockEntities/SkullMagicSpawnerBlockEntity.java
@@ -8,22 +8,19 @@ import com.romanvonklein.skullmagic.networking.ServerPackageSender;
 import com.romanvonklein.skullmagic.util.SpawnerEntry;
 import com.romanvonklein.skullmagic.util.Util;
 
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.particle.FlameParticle;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
-import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
@@ -34,6 +31,7 @@ public class SkullMagicSpawnerBlockEntity extends BlockEntity {
     protected int maxDelay;
     protected int delayed;
     protected int lastParticled;
+    private static final int maxTries = 3;
 
     public SkullMagicSpawnerBlockEntity(BlockPos pos,
             BlockState state) {
@@ -42,6 +40,7 @@ public class SkullMagicSpawnerBlockEntity extends BlockEntity {
 
     public void setSpawnSettings(SpawnerEntry spawn) {
         if (spawn != null) {
+            this.delayed = 0;
             this.spawnCommand = spawn.command;
             this.maxDelay = spawn.maxDelay;
             this.maxSpawns = spawn.maxSpawns;
@@ -50,6 +49,7 @@ public class SkullMagicSpawnerBlockEntity extends BlockEntity {
 
     }
 
+    // TODO: dont spawn when there's loads of mobs around already
     public static void tick(World world, BlockPos pos, BlockState state, BlockEntity ent) {
         if (!world.isClient && ent instanceof SkullMagicSpawnerBlockEntity) {
             SkullMagicSpawnerBlockEntity castedEnt = (SkullMagicSpawnerBlockEntity) ent;
@@ -64,27 +64,39 @@ public class SkullMagicSpawnerBlockEntity extends BlockEntity {
                 SkullMagic.LOGGER.info("Attempting Spawn");
                 Random rand = Random.createLocal();
                 for (int i = 0; i < castedEnt.maxSpawns; i++) {
-                    int posX = pos.getX() + rand.nextBetween(-castedEnt.range, +castedEnt.range);
-                    int posY = pos.getY() + rand.nextBetween(-castedEnt.range, +castedEnt.range);
-                    int posZ = pos.getZ() + rand.nextBetween(-castedEnt.range, +castedEnt.range);
                     ServerCommandSource src = world.getServer().getCommandSource();
                     String command = "FORMATTING FAILED";
-                    if (world.isSpaceEmpty(new Box(posX, posY, posZ, 1, 2, 1))) {
-
-                        try {
-                            SkullMagic.LOGGER.info("preparing spawning command from spawning command: %s",
-                                    castedEnt.spawnCommand);
-                            command = String.format(castedEnt.spawnCommand,
-                                    world.getRegistryKey().getValue().toString(),
-                                    posX, posY,
+                    for (int tryNo = 0; tryNo < maxTries; tryNo++) {
+                        int posX = pos.getX() + rand.nextBetween(-castedEnt.range, +castedEnt.range);
+                        int posY = pos.getY() + rand.nextBetween(-castedEnt.range, +castedEnt.range);
+                        int posZ = pos.getZ() + rand.nextBetween(-castedEnt.range, +castedEnt.range);
+                        if (world.isSpaceEmpty(new Box(posX, posY, posZ, posX + 1, posY + 2, posZ + 1))) {
+                            try {
+                                SkullMagic.LOGGER.info("preparing spawning command from spawning command: {}",
+                                        castedEnt.spawnCommand);
+                                command = String.format(castedEnt.spawnCommand,
+                                        world.getRegistryKey().getValue().toString(),
+                                        posX, posY,
+                                        posZ);
+                                String.format("%d %s", 5, "test");
+                                SkullMagic.LOGGER.info("Using spawning command: {}", command);
+                                world.getServer().getCommandManager().executeWithPrefix(src.withSilent(), command);
+                            } catch (Exception e) {
+                                SkullMagic.LOGGER.warn(
+                                        "Failed formatting or executing command: {}, formatted command: {}",
+                                        castedEnt.spawnCommand, command);
+                                SkullMagic.LOGGER.error(e.getMessage());
+                            }
+                            break;
+                        } else {
+                            SkullMagic.LOGGER.info("Could not find any free space at {} {} {}", posX, posY,
                                     posZ);
-                            SkullMagic.LOGGER.info("Using spawning command: %s", command);
-                            world.getServer().getCommandManager().executeWithPrefix(src.withSilent(), command);
-                        } catch (Exception e) {
-                            SkullMagic.LOGGER.warn("Failed formatting or executing command: %s, formatted command: %s",
-                                    castedEnt.spawnCommand, command);
+                            ServerPackageSender.sendParticleEffectPackageToPlayers(((ServerWorld) world).getPlayers(),
+                                    "minecraft:flame",
+                                    world.getRegistryKey(), new Vec3d(0.5 + posX, 0.5 + posY, 0.5 + posZ));
                         }
                     }
+
                 }
                 world.playSound(null, pos,
                         SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.BLOCKS, 0.5f, 1f);
@@ -118,16 +130,11 @@ public class SkullMagicSpawnerBlockEntity extends BlockEntity {
     @Override
     public void readNbt(NbtCompound tag) {
         if (tag.contains("spawnCommand")) {
-            try {
-                this.range = tag.getInt("range");
-                this.maxSpawns = tag.getInt("maxSpawns");
-                this.maxDelay = tag.getInt("maxDelay");
-                this.delayed = tag.getInt("delayed");
-                this.spawnCommand = tag.getString("spawnCommand");
-            } catch (Exception e) {
-                SkullMagic.LOGGER.error("Failed loading spawnser data from NBT data.");
-            }
-
+            this.range = tag.getInt("range");
+            this.maxSpawns = tag.getInt("maxSpawns");
+            this.maxDelay = tag.getInt("maxDelay");
+            this.delayed = tag.getInt("delayed");
+            this.spawnCommand = tag.getString("spawnCommand");
         }
         super.readNbt(tag);
     }
